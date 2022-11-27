7e3d45d665986dbcfffd58cc6eca05372f82e2de
fbubeck <ferdinand.bubeck@gmx.de>
2022-11-27 18:04:42 +0100

    add preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -4,6 +4,9 @@ import numpy
 
 class iris_detection():
     def __init__(self, image_path):
+        self.tight = None
+        self.mid = None
+        self.wide = None
         self._img_path = image_path
         self._img = None
 
@@ -22,10 +25,22 @@ class iris_detection():
         self._img = cv2.cvtColor(self._img, cv2.COLOR_BGR2GRAY)
         print(self._img)
 
+    # perform Canny-Edge-Detection
+    def detect_edges(self):
+
+        self.wide = cv2.Canny(self._img, 10, 200)
+        self.mid = cv2.Canny(self._img, 30, 150)
+        self.tight = cv2.Canny(self._img, 240, 250)
+
     def store_image(self):
-        cv2.imwrite("data/test-img-gray.png", self._img)
+        cv2.imwrite("data/as-test-gray.png", self._img)
+
+        cv2.imwrite("data/canny-wide.png", self.wide)
+        cv2.imwrite("data/canny-mid.png", self.mid)
+        cv2.imwrite("data/canny-tight.png", self.tight)
 
     def start_detection(self):
         self.load_image()
         self.convert_to_gray_scale()
+        self.detect_edges()
         self.store_image()
