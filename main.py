ac40b919c3a270ff82dced948b3d2f4ec2c712a9
fbubeck <ferdinand.bubeck@gmx.de>
2022-12-15 18:51:28 +0100

    EDD-18

--- main.py
+++ main.py
@@ -3,9 +3,20 @@ import preprocessing
 
 def main():
     print("Starting...")
-    image_path = "data/as-test.jpg"
-    id = preprocessing.iris_detection(image_path)
+
+    image_path1 = "data/as-test1"
+    image_path2 = "data/as-test2"
+    image_path3 = "data/as-test3"
+    typ = "jpg"
+
+    id = preprocessing.iris_detection(image_path1, typ)
     id.start_detection()
+
+    id2 = preprocessing.iris_detection(image_path2, typ)
+    id2.start_detection()
+
+    id3 = preprocessing.iris_detection(image_path3, typ)
+    id3.start_detection()
 
 
 if __name__ == '__main__':
